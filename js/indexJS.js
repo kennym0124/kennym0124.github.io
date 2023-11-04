3863e7e0e2b93df3be98fbc66931bd39665abf01
kennym0124 <117419300+kennym0124@users.noreply.github.com>
2023-11-04 01:07:05 -0400

    Add files via upload
    Website Ver: alpha build 0.1
    
    rudimentary design scheme/concept thought of. currently consists of intro and about me sections.

--- js/indexJS.js
+++ js/indexJS.js
@@ -1,5 +1,13 @@
 let scrollDownButton = document.getElementById('scroll-downButton');
+let aboutMeButton = document.getElementById('aboutMeButton');
+let logo = document.getElementById('logo');
 
+logo.addEventListener("click", () => {
+    document.getElementById('section1').scrollIntoView();
+})
 scrollDownButton.addEventListener("click", () => {
+    document.getElementById('section2').scrollIntoView();
+})
+aboutMeButton.addEventListener("click", () => {
     document.getElementById('section2').scrollIntoView();
 })
